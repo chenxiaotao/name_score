bba28ef8e55c1e6a514a509646f77fff374e15c6
chenxiaotao <chenxiaotao@cd.tuan800.com>
2017-07-21 11:22:21 +0800

    init project

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://gems.ruby-china.org'
+
+gem 'activesupport'
+gem 'mechanize'
+gem 'colorize'
---
+++ auto_run.rb
@@ -0,0 +1,12 @@
+ENV["LANG"]="en_US.UTF-8"
+
+require 'bundler/setup'
+
+require 'active_support'
+require 'active_support/core_ext'
+require 'colorize'
+
+def run
+end
+
+run
